fa2f2093fbcad7b8e196e54f6ba85a56d84fc430
hritsh <hritish.mahajan@gmail.com>
2023-01-11 23:53:59 +0400

    Create Message Input Function

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export const Chat = () => {
-	return <div className="chat-app">Chat</div>;
+	const [newMessage, setNewMessage] = useState("");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(newMessage);
+	};
+	return (
+		<div className="chat-app">
+			<form className="new-message-form" onSubmit={handleSubmit}>
+				<input className="new-message-input" placeholder="Type a message..." onChange={(e) => setNewMessage(e.target.value)} />
+				<button className="send-button" type="submit">
+					Send
+				</button>
+			</form>
+		</div>
+	);
 };
