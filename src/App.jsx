a9c131cf8e768dfd7686e79997fbb58fb9750715
hritsh <hritish.mahajan@gmail.com>
2023-02-08 18:34:49 +0400

    Add Function to Auto Delete Messages after 1 Hour

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,41 @@ import { auth } from "./firebase-config";
 const cookies = new Cookies();
 
 function App() {
-	const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-	const [room, setRoom] = useState(null);
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
 
-	const roomInputRef = useRef(null);
+  const roomInputRef = useRef(null);
 
-	const signUserOut = async () => {
-		await signOut(auth);
-		cookies.remove("auth-token");
-		setIsAuth(false);
-		setRoom(null);
-	};
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  };
 
-	return !isAuth ? (
-		<div>
-			<Auth setIsAuth={setIsAuth} />
-		</div>
-	) : (
-		<>
-			{room ? (
-				<Chat room={room} />
-			) : (
-				<div className="room">
-					<label>Enter Room Name: </label>
-					<input ref={roomInputRef} />
-					<button onClick={() => setRoom(roomInputRef.current.value)}>Join / Create</button>
-				</div>
-			)}
+  return !isAuth ? (
+    <div>
+      <Auth setIsAuth={setIsAuth} />
+    </div>
+  ) : (
+    <>
+      {room ? (
+        <Chat room={room} />
+      ) : (
+        <div className="room">
+          <label>Enter Room Name: </label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Join / Create
+          </button>
+        </div>
+      )}
 
-			<div className="sign-out">
-				<button onClick={signUserOut}>Sign Out</button>
-			</div>
-		</>
-	);
+      <div className="sign-out">
+        <button onClick={signUserOut}>Sign Out</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,62 +1,96 @@
 import { useEffect, useState } from "react";
-import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  where,
+  Timestamp,
+  deleteDoc,
+  getDocs,
+} from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import "../styles/chat.css";
 
 export const Chat = ({ room }) => {
-	const [newMessage, setNewMessage] = useState("");
-	const [messages, setMessages] = useState([]);
-	const messagesRef = collection(db, "messages");
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const messagesRef = collection(db, "messages");
 
-	useEffect(() => {
-		const queryMessages = query(messagesRef, where("room", "==", room), orderBy("createdAt", "asc"));
-		const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-			let messages = [];
-			snapshot.forEach((doc) => {
-				messages.push({ ...doc.data(), id: doc.id });
-			});
-			setMessages(messages);
-		});
-		return () => unsubscribe();
-	}, []);
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt", "asc")
+    );
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+  }, []);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		if (newMessage.trim() === "") return;
+  const deleteOldMessages = async () => {
+    // Get the current timestamp
+    const currentTimestamp = Timestamp.now();
+    // Subtract 1 hour in milliseconds
+    const oneHourAgo = new Timestamp(
+      currentTimestamp.seconds - 5,
+      currentTimestamp.nanoseconds
+    );
+    // Get all messages older than 1 hour
+    const oldMessages = await getDocs(
+      query(messagesRef, where("createdAt", "<", oneHourAgo))
+    );
+    // Delete all old messages
+    oldMessages.forEach(async (message) => {
+      await deleteDoc(message.ref);
+    });
+  };
 
-		await addDoc(messagesRef, {
-			text: newMessage,
-			createdAt: serverTimestamp(),
-			user: auth.currentUser.displayName,
-			room,
-		});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMessage.trim() === "") return;
 
-		setNewMessage("");
-	};
-	return (
-		<div className="chat-app">
-			<div className="header">
-				<h1>Room name: {room.toUpperCase()}</h1>
-			</div>
-			<div className="messages">
-				{messages.map((message) => (
-					<div key={message.id} className="message">
-						<span className="user">{message.user}</span>
-						{message.text}
-					</div>
-				))}
-			</div>
-			<form className="new-message-form" onSubmit={handleSubmit}>
-				<input
-					className="new-message-input"
-					placeholder="Type a message..."
-					onChange={(e) => setNewMessage(e.target.value)}
-					value={newMessage}
-				/>
-				<button className="send-button" type="submit">
-					Send
-				</button>
-			</form>
-		</div>
-	);
+    await addDoc(messagesRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+
+    setNewMessage("");
+    await deleteOldMessages();
+  };
+  return (
+    <div className="chat-app">
+      <div className="header">
+        <h1>Room name: {room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <span className="user">{message.user}</span>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <form className="new-message-form" onSubmit={handleSubmit}>
+        <input
+          className="new-message-input"
+          placeholder="Type a message..."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+        />
+        <button className="send-button" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
