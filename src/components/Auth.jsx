6d2c97206a575a216aaf7c3ba043a40be7192165
hritsh <hritish.mahajan@gmail.com>
2023-02-15 19:02:30 +0400

    Add Anonymous Login Option

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,24 +1,47 @@
 import { auth, provider } from "../firebase-config";
-import { signInWithPopup } from "firebase/auth";
+import {
+  signInWithPopup,
+  signInAnonymously,
+  updateProfile,
+} from "firebase/auth";
 import "../styles/auth.css";
 
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 export const Auth = ({ setIsAuth }) => {
-	const signIn = async () => {
-		try {
-			const result = await signInWithPopup(auth, provider);
-			cookies.set("auth-token", result.user.refreshToken);
-			setIsAuth(true);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	return (
-		<div className="auth">
-			<p>Sign In with Google to Continue</p>
-			<button onClick={signIn}>Sign In</button>
-		</div>
-	);
+  const signIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const signInAnon = async () => {
+    try {
+      const result = await signInAnonymously(auth);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+      // set username for anon user
+      const username = document.getElementById("username").value;
+      if (username) {
+        await updateProfile(auth.currentUser, {
+          displayName: username,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="auth">
+      <p>Sign In with Google to Continue</p>
+      <button onClick={signIn}>Sign In</button>
+      <p>Or Sign In with a Username</p>
+      <input id="username" type="text" />
+      <button onClick={signInAnon}>Sign In</button>
+    </div>
+  );
 };
